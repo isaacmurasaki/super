4d492eeddabc38e7cd2cdb8cc49ffe89d663b516
isaacmurasaki <122030713+isaacmurasaki@users.noreply.github.com>
2023-01-05 21:57:11 +0800

    Update lemon.js

--- lemon.js
+++ lemon.js
@@ -1,8 +1,6 @@
-<script>
 document.addEventListener("DOMContentLoaded", function(){
   const a = document.querySelectorAll("a[href*='isaacmurasaki.lemonsqueezy.com']");
   for (const l of a) {
   l.classList.add("lemonsqueezy-button");
   }
 });
-</script>
